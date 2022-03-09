70da6877430a6874e58898b150e15708ce6111b5
bbashcode <binbash.code@gmail.com>
2022-03-09 06:01:54 +0000

    refactoring based on the final solution

--- index.js
+++ index.js
@@ -1,52 +1,84 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance(){
+  // Calculate the balance using the transaction objects.
+    let status = 0;
+    for(let transaction of this.transactions){
+      status += transaction.value;
+    }
+    return status;
+  }
+
+  addTransaction(transaction){
+    this.transactions.push(transaction);
+  }
+
+
+}
 
 class Transaction {
-  constructor(amount, account){
-    this.amount = amount;
+
+  constructor(amount, account) {
+    this.amount  = amount;
     this.account = account;
   }
-}
 
-class Withdrawal extends Transaction{
-  commit(){
-    this.account.balance -= balance;
+  commit() {
+    if(!this.isAllowed) return false;
+
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
+    return true;
   }
+
 }
 
-class Deposit extends Transaction{
-  commit(){
-    this.account.balance += balance;
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
+
 }
 
-class Account {
-  constructor(username){
-    this.username = username;
-    // Have the account balance start at $0 since that makes more sense.
-    this.balance = 0;
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
   }
+
+  isAllowed(){
+    return (this.account.balance - this.amount >= 0);
+  }
+
 }
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+// DRIVER CODE (yes, keep everything in one file for now... b/c cog load)
+const myAccount = new Account();
 
-// ACCOUNT
-const myAccount = new Account("snow-patrol");
+console.log('Starting Account Balance: ', myAccount.balance);
 
-// WITHDRAWL
-t1 = new Withdrawal(50.25, myAccount);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-console.log('Balance:', balance);
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
 
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
 
-//DEPOSIT
-t3 = new Deposit(120.00);
-t3.commit();
-console.log("Transaction 3: ", t3);
-console.log('Balance:', balance);
+console.log('Account Transaction History: ', myAccount.transactions);
